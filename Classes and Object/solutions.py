30b68340291635efec42766794f8649784a69b5b
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-08-08 19:59:25 +0530

    solution 2 and 3 completed

--- Classes and Object/solutions.py
+++ Classes and Object/solutions.py
@@ -12,3 +12,41 @@ class Person:
 p1 = Person("Ankush",21)
 
 p1.show()
+
+# solution 2
+
+class Circle:
+    def __init__(self,r):
+        self.radius = r
+
+    def setter(self,value):
+        self.radius = value
+
+    def getter(self):
+        print(self.radius)
+c1 = Circle(5)
+c1.setter(15)
+c1.getter()
+
+
+# solution 3
+
+class Rectangle:
+    def __init__(self,l,b):
+        self.length = l
+        self.breath = b
+
+    def setDimenstion(self,l=1,b=1):
+        self.length = l
+        self.breath = b
+
+    def showDimension(self):
+        print(f"Length = {self.length} Breath = {self.breath}")
+
+    def getArea(self):
+        return self.breath * self.length
+
+
+r1 = Rectangle(10,4)
+print(r1.getArea())
+
